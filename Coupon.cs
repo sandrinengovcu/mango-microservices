679ff3e915c0542d14f4a5caee7ae779cc02dda0
sandrinengo <sandrinengo@yahoo.com>
2024-06-10 20:05:51 -0400

    Add project files.

---
+++ Coupon.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+public class CouponDTO
+{
+    public int CouponID { get; set; }
+    public string CouponCode { get; set; }
+    public double DiscountAmount { get; set; }
+    public int MinAmount { get; set; }
+}
---
+++ CouponDTO.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+public class CouponDTO
+{
+	public CouponDTO()
+	{
+	}
+}
